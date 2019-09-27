1d6d9f6cbac2b2d461d5b48bdc87d3f67602a10c
williambl <william@williambl.com>
2019-09-27 22:45:56 +0100

    Improve crop AABBs

--- src/main/java/com/williambl/mawhemenuscrop/BlockMawhemenusCrop.java
+++ src/main/java/com/williambl/mawhemenuscrop/BlockMawhemenusCrop.java
@@ -17,7 +17,7 @@ import java.util.Random;
 public class BlockMawhemenusCrop extends BlockCrops {
 
     private static final PropertyInteger MAWHEMENUS_AGE = PropertyInteger.create("age", 0, 3);
-    private static final AxisAlignedBB[] MAWHEMENUS_AABB = new AxisAlignedBB[] {new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.125D, 1.0D), new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.25D, 1.0D), new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.375D, 1.0D), new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.5D, 1.0D)};
+    private static final AxisAlignedBB[] MAWHEMENUS_AABB = new AxisAlignedBB[] {new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.125D, 1.0D), new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.375D, 1.0D), new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.6875D, 1.0D), new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.6875D, 1.0D)};
 
     BlockMawhemenusCrop() {
         super();
